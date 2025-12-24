406a8b8b29e1f61ac7b64de0448aef9c7bccf911
syams-savero <muhammadsyamssavero@gmail.com>
2025-12-24 21:01:45 +0700

    last

--- ui/src/supabaseClient.ts
+++ ui/src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Ambil URL & Key dari Dashboard Supabase lu (Settings > API)
-const supabaseUrl = '';
-const supabaseAnonKey = '';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
